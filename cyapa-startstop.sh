4df1d6b9c80f7f0bc0d81e49d38c10b112fad476
Teruyuki Imai <teruyuki.imai@gmail.com>
2014-12-27 19:54:50 +0900

    use FreeBSD rc style

--- cyapa-startstop.sh
+++ cyapa-startstop.sh
@@ -1,11 +1,45 @@
 #!/bin/sh
-case $1 in
-start)
+#
+# $FreeBSD$
+#
+
+# PROVIDE: cyapa
+# REQUIRE: hald
+#
+
+. /etc/rc.subr
+
+name="cyapa"
+rcvar="cyapa_enable"
+load_rc_config $name
+
+start_cmd="cyapa_start"
+stop_cmd="cyapa_stop"
+load_cmd="cyapa_loadmod"
+extra_commands="load"
+
+check_and_load()
+{
+	local name=$1
+	kldstat -n $name > /dev/null 2>&1 || kldload $name
+}
+
+cyapa_loadmod()
+{
+	# TODO: I2C bus driver should be configurable
+	check_and_load ig4
+	check_and_load cyapa
+	service moused restart
+}
+
+cyapa_start()
+{
+	cyapa_loadmod
 	ls /dev/cyapa* | while read devfile; do
 		halbasename=`basename "$devfile" | sed -e 's/-/_/g'`
 		udi="/org/freedesktop/Hal/devices/$halbasename"
 		n=0
-		hal-device -a $udi << __END__
+		/usr/local/bin/hal-device -a $udi << __END__
 freebsd.device_file='$devfile'
 freebsd.driver='smb'
 freebsd.unit=$n
@@ -19,13 +53,14 @@ input.x11_driver = 'mouse'
 __END__
 		n=`expr $n + 1`
 	done
-	;;
-stop)
-	lshal | grep "^udi = '/org/freedesktop/Hal/devices/cyapa" | \
+}
+
+cyapa_stop()
+{
+	/usr/local/bin/lshal | grep "^udi = '/org/freedesktop/Hal/devices/cyapa" | \
 	 cut -d= -f2 | sed -e "s/^ *'//; s/'$//" | while read udi; do
-		hal-device -r $udi
+		/usr/local/bin/hal-device -r $udi
 	done
-	;;
-*)
-	echo "usage $0 {start|stop}"
-esac
+}
+
+run_rc_command "$1"
