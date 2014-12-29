2b330ff312a1edd593c7384bd28a1aa5d1ad094b
Teruyuki Imai <teruyuki.imai@gmail.com>
2014-12-29 12:09:57 +0900

    check exisence hal commands

--- cyapa-startstop.sh
+++ cyapa-startstop.sh
@@ -18,6 +18,8 @@ stop_cmd="cyapa_stop"
 load_cmd="cyapa_loadmod"
 extra_commands="load"
 
+halcmddir="/usr/local/bin/"
+
 check_and_load()
 {
 	local name=$1
@@ -36,13 +38,14 @@ cyapa_start()
 {
 	cyapa_loadmod
 	sleep 1
-	ls /dev/cyapa* | while read devfile; do
-		halbasename=`basename "$devfile" | sed -e 's/-/_/g'`
-		udi="/org/freedesktop/Hal/devices/$halbasename"
-		n=0
-		ok=0
-		while [ $ok -eq 0 ];do
-			/usr/local/bin/hal-device -a $udi << __END__
+	if [ -x ${halcmddir}/hal-device ]; then
+		ls /dev/cyapa* | while read devfile; do
+			halbasename=`basename "$devfile" | sed -e 's/-/_/g'`
+			udi="/org/freedesktop/Hal/devices/$halbasename"
+			n=0
+			ok=0
+			while [ $ok -eq 0 ];do
+				${halcmddir}/hal-device -a $udi << __END__
 freebsd.device_file='$devfile'
 freebsd.driver='smb'
 freebsd.unit=$n
@@ -54,22 +57,26 @@ info.udi = '$udi'
 input.device = '/dev/sysmouse'
 input.x11_driver = 'mouse'
 __END__
-			if [ $? = 0 ];then
-				ok=1
-			else
-				sleep 3
-			fi
+				if [ $? = 0 ];then
+					ok=1
+				else
+					sleep 3
+				fi
+			done
+			n=`expr $n + 1`
 		done
-		n=`expr $n + 1`
-	done
+	fi
 }
 
 cyapa_stop()
 {
-	/usr/local/bin/lshal | grep "^udi = '/org/freedesktop/Hal/devices/cyapa" | \
-	 cut -d= -f2 | sed -e "s/^ *'//; s/'$//" | while read udi; do
-		/usr/local/bin/hal-device -r $udi
-	done
+	if [ -x ${halcmddir}/hal-device ]; then
+		${halcmddir}/lshal | \
+		  grep "^udi = '/org/freedesktop/Hal/devices/cyapa" | \
+		  cut -d= -f2 | sed -e "s/^ *'//; s/'$//" | while read udi; do
+		  ${halcmddir}/hal-device -r $udi
+		done
+	fi
 }
 
 run_rc_command "$1"
